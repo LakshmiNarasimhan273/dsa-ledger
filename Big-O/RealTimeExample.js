bce0f8b28a9c00eb543c46fa3c6f59fe9fb9de58
Lakshmi Narasimhan S <bachelorofphotography@gmail.com>
2025-01-18 10:38:00 +0530

    Real time example and time calculation for Big-O

--- Big-O/RealTimeExample.js
+++ Big-O/RealTimeExample.js
@@ -2,16 +2,21 @@
 
 const productData = async () => {
     try{
-        const apiResponse = await fetch('https://fakestoreapi.com/products');
-        const apiData = await apiResponse.json();
+        const apiResponse = await fetch('https://fakestoreapi.com/products'); // Fetching time - O(1) CONSTANT TIME
+        const apiData = await apiResponse.json(); // Parsing time - O(1) CONSTANT TIME
 
-        apiData.forEach((data) => {
-            console.log(data.title);
+        apiData.forEach((data) => { // Looping time - O(n) LINEAR TIME
+            console.log(data.title); // Console time - O(1) CONSTANT TIME
         })
 
     }catch(err){
-        console.error(err);
+        console.error(err); // Console time - O(1) CONSTANT TIME
     }
 }
 
-productData();
+productData(); // Callback time - O(1) CONSTANT TIME
+
+// Totally we have constant time complexity of O(1) + O(1) + O(1) + O(1) + O(1) = O(5) that means 5 constant operations
+// And we have only one Linear time complexity of O(n) that calculates the running time a forEach loop
+// By nature Big O ignores the constant time for the final calculation so that the O(5) constant time will be
+// ignored, so that the final time complexity for this problem is * O(n)
