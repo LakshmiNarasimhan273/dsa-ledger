ea2e0657e82997c51c12969bad37064f5e03ad33
LakshmiNarasimhan273 <bachelorofphotography@gmail.com>
2025-01-20 17:33:20 +0530

    Program optimized

--- Big-O/TimeComplexity.js
+++ Big-O/TimeComplexity.js
@@ -1,15 +1,17 @@
-const basket = ['banana', 'jackfruit','apple', 'pineapple'];
+const basket = ['banana', 'jackfruit','apple', 'pineapple', 'orange', 'jackfruit', 'mango'];
 
 function findFruit(arr){
     let t0 = performance.now();
     for(let i = 0; i < arr.length; i++){
-        if(arr[i] === 'apple'){
+        console.log("Function running");        
+        if(arr[i] === 'pineapple'){
             console.log("Found apple in our basket");
+            break;  // Rule 1 WORST CASE - Modify our program to ensure this function 
+            // terminates as soon as the requirement is met.
         }
     }
     let t1 = performance.now();
-    console.log(`Call to find apple took - ${t0-t1} milliseconds`);
-    
+    console.log(`Call to find pineapple took - ${t0-t1} milliseconds`);   
 }
 
 findFruit(basket);
